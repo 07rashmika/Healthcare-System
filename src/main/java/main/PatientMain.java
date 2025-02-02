3e8530d390fe441d34f41951164d0fc56af94631
loch68 <lochgraphy@gmail.com>
2025-02-03 01:32:13 +0530

    Changes made to the Patient System

---
+++ src/main/java/main/PatientMain.java
@@ -0,0 +1,10 @@
+package main;
+
+import View.PatientView;
+
+public class PatientMain {
+    public static void main(String[] args) {
+        // Launch the Patient Management System
+        new PatientView();
+    }
+}
