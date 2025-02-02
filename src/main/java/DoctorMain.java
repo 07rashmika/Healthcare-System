6f9161115a8c07ee6941721cfda445d42e07db36
AT-Marian <teenamarian@icloud.com>
2025-02-02 18:24:45 +0530

    comment added

--- src/main/java/DoctorMain.java
+++ src/main/java/DoctorMain.java
@@ -2,6 +2,7 @@ import Controller.DoctorController;
 import Model.DoctorModel;
 import View.DoctorView;
 
+//Main method
 public class DoctorMain {
     public static void main(String[] args) {
         DoctorModel model = new DoctorModel();
